8c505112fa3f8dc2385e7bc7b3a849a4de05357b
Milena Ferraz <milenaferraz@gmail.com>
2020-08-28 16:04:40 -0300

    second-commit

---
+++ api/server/src/config/config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "example",
+    "database": "burger_queen",
+    "host": "db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "postgres",
+    "password": "example",
+    "database": "burger_queen_test",
+    "host": "db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "postgres",
+    "password": "example",
+    "database": "burger_quee_production",
+    "host": "db",
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
